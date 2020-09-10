b8b5529921c4ab0f131ebcceeffcde037495a8f4
suchadaK <68003360+suchadaK@users.noreply.github.com>
2020-09-10 21:51:47 +0700

    Add files via upload

---
+++ Bat.java
@@ -0,0 +1,58 @@
+package com.example.game_pong;
+
+import android.graphics.RectF;
+
+class Bat {
+
+    //atttridute
+    private RectF mRect;
+    private float mLength;
+    private float mXCoord;
+    private float mBatSpeed;
+    private int mScreenX;
+    final int STOPPED = 0;
+    final int LEFT = 1;
+    final int RIGHT = 2;
+    private int mBatMoving = STOPPED;
+
+    //constructor
+    Bat(int sx, int sy){
+        mScreenX = sx;
+        mLength = mScreenX / 8;
+        float height = sy / 40;
+        mXCoord = mScreenX / 2;
+        float mYCoord = sy - height;
+        mRect = new RectF(mXCoord, mYCoord,
+                mXCoord + mLength,
+                mYCoord + height);
+        mBatSpeed = mScreenX;
+    }
+
+    void setMovementState(int state){
+        mBatMoving = state;
+    }
+
+    RectF getRect(){
+
+        return mRect;
+    }
+
+    void update(long fps){
+        if(mBatMoving == LEFT){
+            mXCoord = mXCoord - mBatSpeed / fps;
+        }
+        if(mBatMoving == RIGHT){
+            mXCoord = mXCoord + mBatSpeed / fps;
+        }
+        if(mXCoord < 0){
+            mXCoord = 0;
+        }
+        else if(mXCoord + mLength > mScreenX){
+            mXCoord = mScreenX - mLength;
+        }
+        mRect.left = mXCoord;
+        mRect.right = mXCoord + mLength;
+    }
+
+
+}
---
+++ MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.game_pong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+
+public class MainActivity extends AppCompatActivity {
+    //artibute
+    private PongGame mPongGame;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        //initialize  mPongGame
+        mPongGame = new PongGame(this, size.x, size.y);
+        setContentView(mPongGame);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mPongGame.resume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mPongGame.pause();
+
+    }
+}
